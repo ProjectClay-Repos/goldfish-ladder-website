0d1dc0e839ddeb0ef812dd38b0dc79cc04cd3d8c
Adnan <sirgeant10@gmail.com>
2025-05-05 03:21:06 +0600

    Create confounder section.

---
+++ app/(root)/components/Cofounder.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import chatgpt from "@/assets/chatgpt.png";
+
+const Cofounder = () => {
+  return (
+    <section className="max-w-[1080px] mx-auto mt-28">
+      <div className="max-w-[482px] space-y-6 py-10">
+        <h1 className="text-4xl font-bold md:text-5xl lg:text-[56px]">
+          AI Co-Founder
+        </h1>
+      </div>
+      <figure>
+        <Image
+          src={chatgpt}
+          alt="Chat GPT"
+          height={714}
+          width={1070}
+          className="object-contain"
+        />
+      </figure>
+
+      <div className="mt-14 text-center">
+        <button className="bg-[#FFB32C] text-3xl font-bold py-7 px-8 rounded-md">
+          Meet Your AI co-founder
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Cofounder;
