2bf8c37ee30161c905e6deaa33e7e52c38012fdd
Adnan <sirgeant10@gmail.com>
2025-05-05 02:18:25 +0600

    Fix button and remove code.

--- app/(root)/components/Internships.tsx
+++ app/(root)/components/Internships.tsx
@@ -1,7 +1,3 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { ArrowRight } from "lucide-react";
 import InternshipCards from "./cards/InternshipCards";
 
 const Internships = () => {
@@ -9,23 +5,19 @@ const Internships = () => {
     <section className="max-w-[1200px] mx-auto mt-[272px]">
       <div className="mb-12">
         <h2 className="text-6xl font-bold mb-7">Internships</h2>
-        <p className="">lorem ipsum lorem ipsum</p>
+        <p>lorem ipsum lorem ipsum</p>
       </div>
 
-      <div>
+      <div className="overflow-hidden whitespace-nowrap py-2">
         <InternshipCards />
         <InternshipCards />
         <InternshipCards />
       </div>
 
-      <div className="mt-8 text-center">
-        <Link
-          href="/internship"
-          className="inline-flex items-center text-[#6B3F2D] hover:text-[#FDBA3C]"
-        >
-          View all opportunities
-          <ArrowRight className="ml-2 h-4 w-4" />
-        </Link>
+      <div className="mt-8">
+        <button className="bg-[#FFB32C] text-[#6B3F2D] hover:bg-[#FDBA3C]/90 py-3 px-9 text-3xl rounded-2xl font-semibold">
+          View More
+        </button>
       </div>
     </section>
   );
